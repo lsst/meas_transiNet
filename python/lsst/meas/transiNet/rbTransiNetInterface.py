b372d5dd7c56894e32ec706e89c228e0daa19a06
Nima <nima@ap-deeplearning1>
2022-05-04 18:53:40 -0700

    More code style changes

--- python/lsst/meas/transiNet/rbTransiNetInterface.py
+++ python/lsst/meas/transiNet/rbTransiNetInterface.py
@@ -1,40 +1,38 @@
 from model import rbTransiNetModel
+import torch
+
 
 class rbTransiNetInterface:
-    ''' A class for interfacing between the LSST AP pipeline and an rbTransiNet model. '''
-    
+    ''' A class for interfacing between the LSST AP pipeline and
+    an rbTransiNet model. '''
+
     def __init__(self, device='cpu'):
         self.model = rbTransiNetModel
         self.device = device
 
     def init(self, pretrained_file):
 
-        #--- Load pre-trained model from disk
-        network_data = torch.load(pretrained_file,map_location=self.device)
-        self.model.load_state_dict(network_data['state_dict'],strict=True)
+        # --- Load pre-trained model from disk
+        network_data = torch.load(pretrained_file, map_location=self.device)
+        self.model.load_state_dict(network_data['state_dict'], strict=True)
 
-        #--- put model in "eval" mode and stand by
+        # --- put model in "eval" mode and stand by
         self.model.eval()
 
-    def prepare_input(self,x):
+    def prepare_input(self, x):
         ''' 
         Things like format conversion from afw.image.exposure to torch.tensor or
         stacking-up of images can happen here.
         '''
         x = x
         return x
-    
 
-    def infer(self,x):
+    def infer(self, x):
 
-        #--- Perform any required pre-processing and format conversion
+        # --- Perform any required pre-processing and format conversion
         x = self.prepare_input(x)
-        
-        #--- Feed input to the network and get a score
+
+        # --- Feed input to the network and get a score
         score = self.model(x)
 
         return score
-
-    
-
-    
