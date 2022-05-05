ca5fdacccef8ec0daafc3714f6d1dedf75f9cf1b
Nima <nima@ap-deeplearning1>
2022-05-04 18:28:11 -0700

    Initial proposed structure for the interface class

---
+++ python/lsst/meas/transiNet/rbTransiNetInterface.py
@@ -0,0 +1,40 @@
+from model import rbTransiNetModel
+
+class rbTransiNetInterface:
+    ''' A class for interfacing between the LSST AP pipeline and an rbTransiNet model. '''
+    
+    def __init__(self, device='cpu'):
+        self.model = rbTransiNetModel
+        self.device = device
+
+    def init(self, pretrained_file):
+
+        #--- Load pre-trained model from disk
+        network_data = torch.load(pretrained_file,map_location=self.device)
+        self.model.load_state_dict(network_data['state_dict'],strict=True)
+
+        #--- put model in "eval" mode and stand by
+        self.model.eval()
+
+    def prepare_input(self,x):
+        ''' 
+        Things like format conversion from afw.image.exposure to torch.tensor or
+        stacking-up of images can happen here.
+        '''
+        x = x
+        return x
+    
+
+    def infer(self,x):
+
+        #--- Perform any required pre-processing and format conversion
+        x = self.prepare_input(x)
+        
+        #--- Feed input to the network and get a score
+        score = self.model(x)
+
+        return score
+
+    
+
+    
