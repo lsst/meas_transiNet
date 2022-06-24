80bf359aaf6dd634aba955497d70c0ff79370ea0
John Parejko <parejkoj@uw.edu>
2022-06-23 17:21:12 -0700

    Add TransiNetTask and test

---
+++ python/lsst/meas/transiNet/transiNetTask.py
@@ -0,0 +1,142 @@
+# This file is part of meas_transiNet.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__all__ = ["TransiNetTask", "TransiNetConfig"]
+
+import lsst.geom
+import lsst.pex.config
+import lsst.pipe.base
+
+from . import utils
+from . import rbTransiNetInterface
+
+
+class TransiNetConnections(lsst.pipe.base.PipelineTaskConnections,
+                           dimensions=("instrument", "visit", "detector"),
+                           defaultTemplates={"coaddName": "deep", "fakesType": ""}):
+    # NOTE: Do we want the "ready to difference" template, or something
+    # earlier? This one is warped, but not PSF-matched.
+    template = lsst.pipe.base.connectionTypes.Input(
+        doc="Input warped template to subtract.",
+        dimensions=("instrument", "visit", "detector"),
+        storageClass="ExposureF",
+        name="{fakesType}{coaddName}Diff_templateExp"
+    )
+    science = lsst.pipe.base.connectionTypes.Input(
+        doc="Input science exposure to subtract from.",
+        dimensions=("instrument", "visit", "detector"),
+        storageClass="ExposureF",
+        name="{fakesType}calexp"
+    )
+    difference = lsst.pipe.base.connectionTypes.Input(
+        doc="Result of subtracting convolved template from science image.",
+        dimensions=("instrument", "visit", "detector"),
+        storageClass="ExposureF",
+        name="{fakesType}{coaddName}Diff_differenceExp",
+    )
+    diaSources = lsst.pipe.base.connectionTypes.Input(
+        doc="Detected sources on the difference image.",
+        dimensions=("instrument", "visit", "detector"),
+        storageClass="SourceCatalog",
+        name="{fakesType}{coaddName}Diff_diaSrc",
+    )
+
+    # Outputs
+    classifications = lsst.pipe.base.connectionTypes.Output(
+        doc="Catalog of real/bogus classifications for each diaSource, "
+            "element-wise aligned with diaSources.",
+        dimensions=("instrument", "visit", "detector"),
+        storageClass="BaseCatalog",
+        name="{fakesType}{coaddName}realBogusSrc",
+    )
+
+
+class TransiNetConfig(lsst.pipe.base.PipelineTaskConfig, pipelineConnections=TransiNetConnections):
+    modelFile = lsst.pex.config.ChoiceField(
+        dtype=str,
+        doc="TransiNet model to load. This is the name of a python file in the models/ "
+            "directory that contains a single class that is a subclass of `torch.nn.Module`.",
+        allowed={
+            "testModel": "A very basic model, for testing the task and interface.",
+        }
+    )
+    weightsFile = lsst.pex.config.Field(
+        dtype=str,
+        doc="Absolute path to pytorch weights file to load at init.",
+        deprecated="This config is a placeholder while we sort out how to store and load large weight files."
+    )
+    cutoutSize = lsst.pex.config.Field(
+        dtype=int,
+        doc="Width/height of square cutouts to send to classifier.",
+        default=40
+    )
+
+
+class TransiNetTask(lsst.pipe.base.PipelineTask):
+    """Task for running TransiNet real/bogus classification on the output of
+    the image subtraction pipeline.
+    """
+    _DefaultName = "transiNet"
+    ConfigClass = TransiNetConfig
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        model = utils.import_model(self.config.modelFile)
+        self.interface = rbTransiNetInterface.RBTransiNetInterface(model(), self.config.weightsFile)
+
+    def run(self, template, science, difference, diaSources):
+        cutouts = [self._make_cutouts(template, science, difference, source) for source in diaSources]
+        scores = self.interface.infer(cutouts)
+
+        schema = lsst.afw.table.Schema()
+        schema.addField(diaSources.schema["id"].asField())
+        schema.addField("score", doc="real/bogus score of this source", type=float)
+        classifications = lsst.afw.table.BaseCatalog(schema)
+        classifications.resize(len(scores))
+
+        classifications["id"] = diaSources["id"]
+        classifications["score"] = scores
+
+        return lsst.pipe.base.Struct(classifications=classifications)
+
+    def _make_cutouts(self, template, science, difference, source):
+        """Return cutouts of each image centered at the source location.
+
+        Parameters
+        ----------
+        template : `lsst.afw.image.ExposureF`
+        science : `lsst.afw.image.ExposureF`
+        difference : `lsst.afw.image.ExposureF`
+            Exposures to cut images out of.
+        source : `lsst.afw.table.SourceRecord`
+            Source to make cutouts of.
+
+        Returns
+        -------
+        cutouts, `lsst.meas.transiNet.CutoutInputs`
+            Cutouts of each of the input images.
+        """
+        extent = lsst.geom.Extent2I(self.config.cutoutSize)
+        box = lsst.geom.Box2I.makeCenteredBox(source.getCentroid(), extent)
+        return rbTransiNetInterface.CutoutInputs(science=science.Factory(science, box).image.array,
+                                                 template=template.Factory(template, box).image.array,
+                                                 difference=difference.Factory(difference, box).image.array)
---
+++ tests/test_TransiNetTask.py
@@ -0,0 +1,95 @@
+# This file is part of meas_transiNet.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os.path
+import unittest
+import numpy as np
+
+import lsst.afw.table
+from lsst.geom import Point2I, Point2D, Box2I
+import lsst.meas.base.tests
+import lsst.utils.tests
+
+from lsst.meas.transiNet import TransiNetTask
+
+
+class TestTransiNetTask(lsst.utils.tests.TestCase):
+    def setUp(self):
+        bbox = Box2I(Point2I(0, 0), Point2I(400, 400))
+        dataset = lsst.meas.base.tests.TestDataset(bbox)
+        dataset.addSource(5000, Point2D(50, 50.))
+        # TODO: make one of these centered in a different corner of the pixel,
+        # to test that the cutout is properly centered.
+        dataset.addSource(10000, Point2D(100, 50.))
+        self.exposure, self.catalog = dataset.realize(10.0, dataset.makeMinimalSchema())
+
+        torch_data = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data/checkpoint.pth.zip")
+        self.config = TransiNetTask.ConfigClass()
+        self.config.modelFile = "testModel"
+        self.config.weightsFile = torch_data
+
+    def test_make_cutouts(self):
+        task = TransiNetTask(config=self.config)
+        for record in self.catalog:
+            result = task._make_cutouts(self.exposure, self.exposure, self.exposure, record)
+            self._check_cutout(result.science, task.config.cutoutSize)
+            self._check_cutout(result.template, task.config.cutoutSize)
+            self._check_cutout(result.difference, task.config.cutoutSize)
+
+    def _check_cutout(self, image, size):
+        """Test that the image cutout was made correctly.
+
+        Parameters
+        ----------
+        image : `np.ndarray` (N, 2)
+            Square cutout made from image.
+        size : `int`
+            Expected size of the cutout.
+        """
+        self.assertEqual(image.shape, (size, size))
+        max_index = np.unravel_index(image.argmax(), image.shape)
+        # TODO: I'm not comfortable with this particular test: the exact
+        # position of the max pixel depends on where in that pixel the
+        # centroid is. We can assume Box2I.makeCenteredBox works correctly...
+        self.assertEqual(max_index, ((size/2)-1, (size/2)-1))
+
+    def test_run(self):
+        """Test that run passes an appropriate object to the interface,
+        mocking the interface infer step so we don't need to use pytorch.
+        """
+        scores = np.array([0.0, 1.0])
+        task = TransiNetTask(config=self.config)
+        task.interface.infer = unittest.mock.Mock(task.interface.infer)
+        with unittest.mock.patch.object(task.interface, "infer") as mock_infer:
+            mock_infer.return_value = scores
+            result = task.run(self.exposure, self.exposure, self.exposure, self.catalog)
+            self.assertIsInstance(result.classifications, lsst.afw.table.BaseCatalog)
+            np.testing.assert_array_equal(self.catalog["id"], result.classifications["id"])
+            np.testing.assert_array_equal(scores, result.classifications["score"])
+
+
+class MemoryTester(lsst.utils.tests.MemoryTestCase):
+    pass
+
+
+if __name__ == "__main__":
+    lsst.utils.tests.init()
+    unittest.main()
