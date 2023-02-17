8779710411a7656b5e0ce7144ba633b47bdf9ebb
Nima <nimaseda@uw.edu>
2023-02-16 18:22:02 -0800

    storageAdapter now incorporates a dict-based mapping from strings to classes; an endeavor to mitigate potential source injection attacks!

--- python/lsst/meas/transiNet/modelPackages/storageAdapter.py
+++ python/lsst/meas/transiNet/modelPackages/storageAdapter.py
@@ -1,26 +1,44 @@
-# StorageAdapter: a class factory for creating two types
-# of storage adapters: StorageAdapterLocal and SotrageAdapterNeighbor.
-# These represent possible storage types/modes for ModelPackages.
-
 from .storageAdapterLocal import StorageAdapterLocal
 from .storageAdapterNeighbor import StorageAdapterNeighbor
 
 
 class StorageAdapter:
+    """A class factory for creating two types of storage adapters:
+    StorageAdapterLocal and SotrageAdapterNeighbor. These represent
+    possible storage types/modes for ModelPackages.
+    """
+
+    # A dict mapping storage modes to storage adapter class.
+    #
+    # This is necessary to guarantee that the user does not
+    # specify a too customized storage mode -- to try to prevent
+    # source injection attacks.
+    storageAdapterClasses = {
+        'local': StorageAdapterLocal,
+        'neighbor': StorageAdapterNeighbor,
+    }
 
     @classmethod
     def create(cls, modelPackageName, storageMode):
-        '''
-        Factory method for creating a storage adapter,
+        """ Factory method to create a storage adapter
         based on the storageMode parameter.
 
-        Returns:
-            A storage adapter object.
-        '''
+        Parameters
+        ----------
+        modelPackageName : `str`
+            The name of the model package.
+        storageMode : `str`
+            The storage mode for the model package.
 
-        if storageMode == 'local':
-            return StorageAdapterLocal(modelPackageName)
-        elif storageMode == 'neighbor':
-            return StorageAdapterNeighbor(modelPackageName)
-        else:
-            raise Exception("Unknown storage mode: " + storageMode)
+        Returns
+        -------
+        storageAdapter : {StorageAdapterLocal, StorageAdapterNeighbor}
+            A storage adapter object, based on the storageMode parameter.
+        """
+        # Check that the storage mode is valid.
+        if storageMode not in cls.storageAdapterClasses:
+            raise ValueError('Invalid storage mode: ' + storageMode)
+
+        # Create and return the storage adapter.
+        storageAdapter = cls.storageAdapterClasses[storageMode](modelPackageName)
+        return storageAdapter
