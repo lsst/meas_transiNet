aa56333d649cef896f2efdd6fb39588fc4c4a1b1
Nima <nimaseda@uw.edu>
2023-02-16 18:22:02 -0800

    storageAdapter now incorporates a dict-based mapping from strings to classes; an endeavor to mitigate potential source injection attacks!

--- python/lsst/meas/transiNet/modelPackages/storageAdapter.py
+++ python/lsst/meas/transiNet/modelPackages/storageAdapter.py
@@ -8,8 +8,18 @@ from .storageAdapterNeighbor import StorageAdapterNeighbor
 
 class StorageAdapter:
 
+    # A dict mapping storage modes to storage adapter class.
+    #
+    # This is necessary to guarantee that the user does not
+    # specify a too customized storage mode -- to try to prevent
+    # source injection attacks.
+    storageAdapterClasses = {
+        'local': StorageAdapterLocal,
+        'neighbor': StorageAdapterNeighbor,
+    }
+
     @classmethod
-    def create(cls, modelPackageName, storageMode):
+    def create(cls, modelPackageName, storageMode, **kwargs):
         '''
         Factory method for creating a storage adapter,
         based on the storageMode parameter.
@@ -18,9 +28,9 @@ class StorageAdapter:
             A storage adapter object.
         '''
 
-        if storageMode == 'local':
-            return StorageAdapterLocal(modelPackageName)
-        elif storageMode == 'neighbor':
-            return StorageAdapterNeighbor(modelPackageName)
-        else:
-            raise Exception("Unknown storage mode: " + storageMode)
+        # Check that the storage mode is valid.
+        if storageMode not in cls.storageAdapterClasses:
+            raise ValueError('Invalid storage mode: ' + storageMode)
+
+        # Create the storage adapter in one line.
+        return cls.storageAdapterClasses[storageMode](modelPackageName, **kwargs)
