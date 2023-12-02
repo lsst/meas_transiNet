cfa856795e966c468d9d8fdeb3fc6a7db668781b
Nima <nimaseda@uw.edu>
2023-12-01 18:34:50 -0800

    Add the static ingest() method, as a convenience tool for ingesting new model packages.

--- python/lsst/meas/transiNet/modelPackages/storageAdapterButler.py
+++ python/lsst/meas/transiNet/modelPackages/storageAdapterButler.py
@@ -1,9 +1,13 @@
 from .storageAdapterBase import StorageAdapterBase
+from lsst.meas.transiNet.modelPackages.formatters import BinaryFormatter
+from lsst.daf.butler import DatasetType, FileDataset, DatasetRef
 from . import utils
 
 import torch
 import zipfile
+import tempfile
 import io
+import os
 import yaml
 
 __all__ = ["StorageAdapterButler"]
@@ -100,7 +104,6 @@ class StorageAdapterButler(StorageAdapterBase):
             # fetch the object using butler
             self.butler_loaded_package = self.butler.get(list(results)[0])
 
-
         # The object in the memory is a zip file. We need to decompress it.
         with zipfile.ZipFile(self.butler_loaded_package, 'r') as zip_ref:
             with zip_ref.open('checkpoint') as f:
@@ -177,3 +180,65 @@ class StorageAdapterButler(StorageAdapterBase):
 
         metadata = yaml.safe_load(self.metadata_file)
         return metadata
+
+    @staticmethod
+    def ingest(model_package, butler, model_package_name=None):
+        """
+        Ingest a model package to the butler repository.
+
+        Parameters
+        ----------
+        model_package : nnModelPackage
+            The model package to be ingested.
+        butler : `lsst.daf.butler.Butler`
+            The butler instance to use for ingesting.
+        model_package_name : `str`, optional
+            The name of the model package to be ingested.
+        """
+
+        # Check if the input model package is of a proper type.
+        if model_package.adapter is StorageAdapterButler:
+            raise ValueError('The input model package cannot be of the butler type')
+
+        # Choose the name of the model package to be ingested.
+        if model_package_name is None:
+            the_name = model_package.model_package_name
+        else:
+            the_name = model_package_name
+
+        # Create the destination run collection.
+        run_collection = StorageAdapterButler.packages_parent_collection+"/"+the_name
+        butler.registry.registerRun(run_collection)
+
+        # Create the dataset type (and register it, just in case).
+        data_id = {}
+        dataset_type = DatasetType(StorageAdapterButler.dataset_type_name,
+                               dimensions=[],
+                               storageClass="ModelPackage",
+                               universe=butler.registry.dimensions)
+
+        def register_dataset_type(butler, dataset_type_name, dataset_type):
+            try: # Do nothing if the dataset type is already registered
+                butler.registry.getDatasetType(dataset_type_name)
+            except KeyError:
+                butler.registry.registerDatasetType(dataset_type)
+
+        register_dataset_type(butler, StorageAdapterButler.dataset_type_name, dataset_type)
+
+        # Create a temporary file and Zip all the three components into it.
+        temp_fd, temp_path = tempfile.mkstemp(suffix='.bin')
+        with os.fdopen(temp_fd, 'wb') as tmp:
+            with zipfile.ZipFile(tmp, mode="w", compression=zipfile.ZIP_DEFLATED) as zf:
+                zf.write(model_package.adapter.checkpoint_filename, arcname="checkpoint")
+                zf.write(model_package.adapter.model_filename, arcname="architecture")
+                zf.write(model_package.adapter.metadata_filename, arcname="metadata")
+
+        file_dataset = FileDataset(path=temp_path,
+                                   refs=DatasetRef(dataset_type, data_id, run=run_collection),
+                                   formatter=BinaryFormatter)
+
+        # Ingest the file into the butler repo
+        butler.ingest(file_dataset, transfer='copy')
+
+        # Remove the temporary file
+        os.remove(temp_path)
