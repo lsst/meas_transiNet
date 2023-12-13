ff1c4c560cb512b43f043fd1cf68f7453d5fc0c0
Nima <nimaseda@uw.edu>
2023-12-12 17:32:01 -0800

    Add fromBytes(), toBytes() to NNModelPackageFormatter.

--- python/lsst/meas/transiNet/modelPackages/formatters.py
+++ python/lsst/meas/transiNet/modelPackages/formatters.py
@@ -19,11 +19,67 @@ class NNModelPackageFormatter(Formatter):
     extension = ".zip"
 
     def read(self, component=None):
+        """Read a dataset.
+
+        Parameters
+        ----------
+        component : `str`, optional
+            Component to read from the file.
+
+        Returns
+        -------
+        payload : `NNModelPackagePayload`
+            The requested data as a Python object.
+        """
         payload = NNModelPackagePayload()
         with open(self.fileDescriptor.location.path, "rb") as f:
             payload.bytes = BytesIO(f.read())
         return payload
 
     def write(self, inMemoryDataset):
+        """Write a Dataset.
+
+        Parameters
+        ----------
+        inMemoryDataset : `object`
+            The Dataset to store.
+
+        """
         with open(self.fileDescriptor.location.path, "wb") as f:
             f.write(inMemoryDataset.bytes.getvalue())
+
+    def fromBytes(self, serializedDataset: bytes, component=None):
+        """Read serialized data into a Dataset or its component.
+
+        Parameters
+        ----------
+        serializedDataset : `bytes`
+            Bytes object to unserialize.
+        component : `str`, optional
+            Component to read from the Dataset. Only used if the `StorageClass`
+            for reading differed from the `StorageClass` used to write the
+            file.
+
+        Returns
+        -------
+        payload : `NNModelPackagePayload`
+            The requested data as a Python object.
+        """
+        payload = NNModelPackagePayload()
+        payload.bytes = BytesIO(serializedDataset)
+        return payload
+
+    def toBytes(self, inMemoryDataset):
+        """Serialize the Dataset to bytes based on formatter.
+
+        Parameters
+        ----------
+        inMemoryDataset : `object`
+            The Python object to serialize.
+
+        Returns
+        -------
+        serializedDataset : `bytes`
+            Bytes representing the serialized dataset.
+        """
+        return inMemoryDataset.bytes.getvalue()
